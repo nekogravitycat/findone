ca20846dcceb6c88ae5aefc51e6ef950e39e1aef
viiccwen <vic20050418@gmail.com>
2025-04-28 16:55:37 +0800

    feat: change return type from array to json

---
+++ server/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace server.Models
+{
+    public class Response
+    {
+        public class CreateRoomResponse
+        {
+            [JsonPropertyName("roomId")]
+            public required string RoomId { get; set; }
+            [JsonPropertyName("userId")]
+            public Guid UserId { get; set; }
+        }
+    }
+}
