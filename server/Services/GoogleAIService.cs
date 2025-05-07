141c0e1a7dd8d63bc6660fa41611bc65f5720299
viiccwen <vic20050418@gmail.com>
2025-05-07 10:25:08 +0800

    fix: model define Repeated

--- server/Services/GoogleAIService.cs
+++ server/Services/GoogleAIService.cs
@@ -4,23 +4,26 @@ using System.Text.Json;
 
 namespace server.Services
 {
-    public class GoogleAIService
-    {
-        private readonly string? _apiKey;
-        private readonly string _systemInstruction =
-            "You are an image recognition judge in a fun multiplayer game. " +
-            "Each round, players submit images to match quirky prompts." +
-            "Your task is to:\r\n" +
-            "1. Evaluate whether the image matches the prompt.\r\n" +
-            "2. Respond with:\r\n" +
-            "   - A boolean indicating if the image matches the prompt(`true` or `false`)\r\n" +
-            "   - A short, playful zh-tw comment(e.g., “恨帥潮” or “能拍出這張圖，也是挺有生活的”)\r\n" +
-            "\r\n" +
-            "Be lighthearted but clear." +
-            "Responses should be fun and understandable by players." +
-            "Avoid offensive or overly negative language.\r\n";
+  public class GoogleAIService
+  {
+    private readonly string? _apiKey;
+    private readonly GoogleAI _googleAI;
+    private readonly GenerativeModel _model;
+    private readonly GenerateContentConfig _generationConfig;
+    private readonly string _systemInstruction =
+        "You are an image recognition judge in a fun multiplayer game. " +
+        "Each round, players submit images to match quirky prompts." +
+        "Your task is to:\r\n" +
+        "1. Evaluate whether the image matches the prompt.\r\n" +
+        "2. Respond with:\r\n" +
+        "   - A boolean indicating if the image matches the prompt(`true` or `false`)\r\n" +
+        "   - A short, playful zh-tw comment(e.g., “恨帥潮” or “能拍出這張圖，也是挺有生活的”)\r\n" +
+        "\r\n" +
+        "Be lighthearted but clear." +
+        "Responses should be fun and understandable by players." +
+        "Avoid offensive or overly negative language.\r\n";
 
-        private readonly string _responseSchema = @"
+    private readonly string _responseSchema = @"
         {
           ""type"": ""object"",
           ""properties"": {
@@ -34,27 +37,27 @@ namespace server.Services
           ""required"": [""match"", ""comment""]
         }";
 
-        public GoogleAIService(IConfiguration configuration)
-        {
-            _apiKey = configuration["APIKeys:GOOGLE_API_KEY"]
-                ?? throw new ArgumentNullException("GOOGLE_API_KEY", "Missing GOOGLE_API_KEY in configuration."); ;
-        }
-
-        public async Task<ImageResponse?> AnalyzeImage(string base64Image, string mimeType, string clientRequest)
-        {
-            var googleAI = new GoogleAI(_apiKey);
-            var systemInstruction = new Content(_systemInstruction);
-            var model = googleAI.GenerativeModel(Model.Gemini20Flash, systemInstruction: systemInstruction);
-            var generationConfig = new GenerateContentConfig
-            {
-                ResponseMimeType = "application/json",
-                ResponseSchema = _responseSchema
-            };
-            var query = new GenerateContentRequest(clientRequest, generationConfig);
-            await query.AddMedia(base64Image, mimeType: mimeType);
-
-            var response = await model.GenerateContent(query);
-            return string.IsNullOrEmpty(response.Text) ? null : JsonSerializer.Deserialize<ImageResponse>(response.Text);
-        }
+    public GoogleAIService(IConfiguration configuration)
+    {
+      _apiKey = configuration["APIKeys:GOOGLE_API_KEY"]
+          ?? throw new ArgumentNullException("GOOGLE_API_KEY", "Missing GOOGLE_API_KEY in configuration.");
+      _googleAI = new GoogleAI(_apiKey);
+      _model = _googleAI.GenerativeModel(Model.Gemini20Flash, systemInstruction: new Content(_systemInstruction));
+      _generationConfig = new GenerateContentConfig
+      {
+        ResponseMimeType = "application/json",
+        ResponseSchema = _responseSchema
+      };
     }
+
+    public async Task<ImageResponse?> AnalyzeImage(string base64Image, string mimeType, string clientRequest)
+    {
+      GenerateContentRequest query = new GenerateContentRequest(clientRequest, _generationConfig);
+      await query.AddMedia(base64Image, mimeType: mimeType);
+
+      GenerateContentResponse response = await _model.GenerateContent(query);
+      response.CheckResponse();
+      return string.IsNullOrEmpty(response.Text) ? null : JsonSerializer.Deserialize<ImageResponse>(response.Text);
+    }
+  }
 }
